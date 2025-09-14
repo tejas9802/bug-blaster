fa2ac3c2f94427a75d35518d0d7dd0989f68288b
Tejas B <tejasbarange@gmail.com>
2025-09-14 17:43:54 +0530

    changes in title

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -30,7 +30,7 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Bug Blaster</h1>
+        <h1>Bug Tracker</h1>
         <TicketForm
           key={state.tickets._id}
           dispatch={dispatch}
